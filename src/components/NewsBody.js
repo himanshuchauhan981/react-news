8265682c21dc58c5d003ab72ed83c1e1e3218aca
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-12-18 23:13:13 +0530

    created source component

--- src/components/NewsBody.js
+++ src/components/NewsBody.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Sources from './Sources';
 
 const NewsBody = () => {
 	return (
 		<div className='mt-6'>
-			<div className='row'>
-				<div className='col-md-3'>helllo</div>
+			<div className='container-fluid h-100'>
+				<div className='row h-100'>
+					<div className='col-md-2 bg-secondary p-0'>
+						<Sources />
+					</div>
+					<div className='col-md-5'></div>
+					<div className='col-md-5'></div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Sources.js
+++ src/components/Sources.js
@@ -1,1 +1,86 @@
 import React from 'react';
+import axios from 'axios';
+import {
+	Collapse,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+} from '@material-ui/core';
+import {
+	Star,
+	ExpandMore,
+	AddLocation,
+	ExpandLess,
+	OpenInNew,
+} from '@material-ui/icons';
+
+class Sources extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			sources: [],
+			open_sources: false,
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get(
+				'https://newsapi.org/v2/sources?apiKey=d3e7f873a13948c283ec916cd261e273'
+			)
+			.then((res) => {
+				let sources = res.data.sources.slice(5, 10);
+				let sources_list = sources.map((source) => ({
+					id: source.id,
+					name: source.name,
+					url: source.url,
+				}));
+				this.setState({ sources: sources_list });
+			});
+	}
+
+	handle_sources = () => {
+		this.setState({
+			open_sources: !this.state.open_sources,
+		});
+	};
+
+	render() {
+		let { sources, open_sources } = this.state;
+		let sources_list = sources.map((source) => (
+			<ListItem button className='pl-4'>
+				<ListItemText primary={source.name} />
+				<OpenInNew
+					fontSize='small'
+					onClick={() => window.open(source.url)}
+				/>
+			</ListItem>
+		));
+		return (
+			<List component='nav'>
+				<ListItem button>
+					<ListItemIcon>
+						<Star />
+					</ListItemIcon>
+					<ListItemText>Favorites</ListItemText>
+					<ExpandMore />
+				</ListItem>
+				<ListItem button onClick={this.handle_sources}>
+					<ListItemIcon>
+						<AddLocation />
+					</ListItemIcon>
+					<ListItemText>Sources</ListItemText>
+					{open_sources ? <ExpandLess /> : <ExpandMore />}
+				</ListItem>
+				<Collapse in={open_sources} timeout='auto' unmountOnExit>
+					<List component='div' disablePadding>
+						{sources_list}
+					</List>
+				</Collapse>
+			</List>
+		);
+	}
+}
+
+export default Sources;
