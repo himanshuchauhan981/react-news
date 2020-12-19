bfc19c62949b8471f9c65bf7dc6f3c3d6c1abe16
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-12-19 11:13:57 +0530

    added news header component

---
+++ src/components/NewsBody.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewsBody = () => {
+	return (
+		<div className='mt-6'>
+			<div className='container-fluid h-100'>
+				<div className='row h-100'>
+					<div className='col-md-2 bg-secondary p-0'></div>
+					<div className='col-md-5'></div>
+					<div className='col-md-5'></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NewsBody;
